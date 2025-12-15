5bb0c72d11ad094476cd38017dfa2430d4663d87
Bohdan Sharubin <Bogdansh13@gmail.com>
2025-12-15 22:10:23 +0200

    Adding new page as blog page and implement find function with js

---
+++ scripts/posts.js
@@ -0,0 +1,60 @@
+const articles = [
+  {
+            id: 1,
+            title: "Fullstack-developer master class",
+            author: "Bohdan Sharubin",
+            date: "2025-12-15",
+            category: "WEB",
+            tags: [
+                "WEB",
+                "Fullstack",
+                "Master of HTML",
+                "Basics"
+            ],
+            content: "Cookbook for web programming from master of javascript "
+        },
+        {
+            id: 2,
+            title: "Java for rookies",
+            author: "Bohdan Sharubin",
+            date: "2025-12-15",
+            category: "Java",
+            tags: [
+                "Java",
+                "Java SE",
+                "Algorithms",
+                "Basics"
+            ],
+            content: "Java essenthial course. Includes basics, exceptions, IO streams "
+        },
+        {
+            id: 3,
+            title: "Spring framework",
+            author: "Robert C. Martin",
+            date: "2025-12-15",
+            category: "Spring",
+            tags: [
+                "WEB",
+                "Fullstack",
+                "Java EE",
+                "Advanced"
+            ],
+            content: "Speaking about Spring framework, Spring Core and using it with Java"
+        },
+
+  {
+    id: 4,
+    title: "Advanced Java",
+    author: "Bohdan Sharubin",
+    date: "2025-12-13",
+    category: "Java",
+    tags: ["Java", "Concurrency", "Advanced", "Threads"],
+    content:
+      "Java advanced topics includes concurrency, Executors, Bariers and more another interesting things",
+  },
+];
+export default articles;
+
+
+
+
